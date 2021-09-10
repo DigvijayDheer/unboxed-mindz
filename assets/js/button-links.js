1256bae0a1e7104539af253a13778a8c63df3e52
DigvijayDheer <digvijaydheersaini@gmail.com>
2021-09-10 19:05:48 +0530

    add button-links.js

---
+++ assets/js/button-links.js
@@ -0,0 +1,27 @@
+document.getElementById("enrollNowButton").onclick = function () {
+    location.href = "https://docs.google.com/forms/d/e/1FAIpQLSeaDlssH5ltINlZJKugteHz3HfWV16gN3oebrLQ8xDej4U8FQ/viewform?usp=sf_link";
+};
+
+document.getElementById("freeTrialButton").onclick = function () {
+    location.href = "https://docs.google.com/forms/d/e/1FAIpQLSflp_emGsXbwZjh5Vv4UALjxNknGxstbE2lWe3kQfOeSiFu0w/viewform?usp=sf_link";
+};
+
+document.getElementById("freeTrialButtonTwo").onclick = function () {
+    location.href = "https://docs.google.com/forms/d/e/1FAIpQLSflp_emGsXbwZjh5Vv4UALjxNknGxstbE2lWe3kQfOeSiFu0w/viewform?usp=sf_link";
+};
+
+document.getElementById("freeTrialButtonThree").onclick = function () {
+    location.href = "https://docs.google.com/forms/d/e/1FAIpQLSflp_emGsXbwZjh5Vv4UALjxNknGxstbE2lWe3kQfOeSiFu0w/viewform?usp=sf_link";
+};
+
+document.getElementById("basicClassButton").onclick = function () {
+    location.href = "https://www.instamojo.com/@unboxedmindz/lde96f5167bfc4bc18690f1e3df06b629/";
+};
+
+document.getElementById("comprehensiveClassButton").onclick = function () {
+    location.href = "https://www.instamojo.com/@unboxedmindz/la9f4bdea22c541aca4ce1d239cb25c65/";
+};
+
+document.getElementById("extensiveClassButton").onclick = function () {
+    location.href = "https://www.instamojo.com/@unboxedmindz/lefea9b053f5f47669b33b0edfa2f6b64/";
+};
